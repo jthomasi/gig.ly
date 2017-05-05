92b2d3be73ee205b878357a8c0a1a68699229eed
afordfa <anitamford@gmail.com>
2017-05-04 19:14:55 -0500

    pushing models, routes and server.js

---
+++ models/admins.js
@@ -0,0 +1,39 @@
+module.exports = function(sequelize, DataTypes) {
+  var Admin = sequelize.define("Admin", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 100]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1, 100]
+    },
+    cellPhone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      len: [10, 10]
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      len: [1, 20]
+    }
+  },
+    {
+      // We're saying that we want our Author to have Posts
+      classMethods: {
+        associate: function(models) {
+          // An Author (foreignKey) is required or a Post can't be made
+          Admin.hasMany(models.Event, {
+            onDelete: "cascade"
+          });
+        }
+      }
+    }
+  );
+  return Admin;
+};
