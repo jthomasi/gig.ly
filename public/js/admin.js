e99c0f08ec7c550fe05941f2b967b7c66a93377c
Jacob Wallace <jacob.wallace@utexas.edu>
2017-05-06 11:07:11 -0500

    html-routes tweaks, /admin to get to admin main page for now

---
+++ public/js/admin.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+	$('#calendar').fullCalendar({
+    	events: []
+	});
+
+	//example array that would be in a DB
+	//will need to use either MySQL or
+	//local storage to test
+	var gigArray  = [
+		        {
+		            title  : "newGig.gigName",
+		            start  : '2017-05-05T12:30:00',
+		            //end	: '2017-05-09T14:30:00',
+		            allDay : false // will make the time show
+		        }
+		    ]
+
+	$('#calendar').fullCalendar( 'addEventSource', gigArray );		    
+
+	$("#createGig").on("click", function(event){
+
+		event.preventDefault();
+
+		var gigName = $("#gigName").val().trim();
+		var gigLocation = $("#gigLocation").val().trim();
+		var gigDate = $("#gigDate").val().trim();
+		//will need to separate into:
+		// var gigYearStart = $("#gigYear")
+		// var gigMonthStart =
+		// var gigDayStart = 
+		// var gigHourStart = 
+		// var gigMinuteStart =
+		// var gigSecondStart =
+		// var gigYearEnd = $("#gigYear")
+		// var gigMonthEnd =
+		// var gigDayEnd = 
+		// var gigHourEnd = 
+		// var gigMinuteEnd =
+		// var gigSecondEnd =
+		var TESTgigHours = $("#gigHours").val().trim();
+		var gigText = $("#gigText").val().trim();
+
+		//so i.e. start: gigYearStart + "-" + gigMonthStart + "-" gigDayStart + "T" + gigHourStart + ":" + gigMinuteStart + ":" + gigSecondStart
+		var newGig = {
+			title: gigName,
+			//location: gigLocation,
+			start: '2017-05-07T12:30:00',
+			//hours: gigHours,
+			//text: gigText
+			allDay: false
+		}
+
+		gigArray.push(newGig);
+
+		$('#calendar').fullCalendar( 'removeEventSources' );
+		//remove event sources because we we are pushing one object
+		//to an array and we will reload entire array of objects to
+		//calendar. W/o removing we will have duplicate events
+
+		$('#calendar').fullCalendar( 'addEventSource', gigArray );
+
+		console.log(newGig);
+
+	});
+
+});
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,12 +5,12 @@ var path = require("path");
 module.exports = function(app) {
 
 	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname, "./public/index.html"));
+		res.sendFile(path.join(__dirname, "../public/index.html"));
 	});
 
   // cms route loads cms.html
 	app.get("/admin", function(req, res) {
-		res.sendFile(path.join(__dirname, "admin.html"));
+		res.sendFile(path.join(__dirname, "../public/admin.html"));
 	});
 
 
