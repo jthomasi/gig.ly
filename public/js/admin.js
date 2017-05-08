accdedcffcdc63682afaad0fae47917d0715316c
Jacob Wallace <jacob.wallace@utexas.edu>
2017-05-08 00:17:29 -0500

    can now add PM times to start

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
+	//initialize starting time for gig input box as a timepicker
 	$('#startTime').timepicker({ 'scrollDefault': 'now' });
-
+	//initialize end time for gig input box as a timepicker
+	$('#endTime').timepicker({ 'scrollDefault': 'now' });
+	//initialize full calendar to prepare to recieve event data
 	$('#calendar').fullCalendar({
     	events: []
 	});
@@ -13,7 +16,7 @@ $(document).ready(function(){
 		        {
 		            title  : "Test Gig",
 		            start  : '2017-05-05T02:30:00',
-		            //end	: '2017-05-09T14:30:00',
+		            end	: '2017-05-05T14:30:00',
 		            allDay : false // will make the time show
 		        }
 		    ]
@@ -29,16 +32,10 @@ $(document).ready(function(){
 		var gigYear = $("#datepicker").val()[6] + $("#datepicker").val()[7] + $("#datepicker").val()[8] + $("#datepicker").val()[9];
 		var gigMonth = $("#datepicker").val()[0] + $("#datepicker").val()[1];
 		var gigDay = $("#datepicker").val()[3] + $("#datepicker").val()[4];
-		//var gigHourStart = $("#startTime").val();
-		//var gigMinuteStart = $("#gigMinuteStart").val();
-		//var gigSecondStart = $("#gigSecondStart").val();
-		// var gigHourEnd = 
-		// var gigMinuteEnd =
-		// var gigSecondEnd =
-		//var TESTgigHours = $("#gigHours").val().trim();
+		var gigStart = $("#startTime");
+		var gigEnd = $("#endTime");
 		var gigText = $("#gigText").val().trim();
 
-		//so i.e. 
 		var startString = '';
 		startString += gigYear;
 		startString += '-';
@@ -46,31 +43,129 @@ $(document).ready(function(){
 		startString += '-';
 		startString += gigDay;
 		startString += 'T';
-		//startString += '02:30:00';
-		//gigHourStart;
-		//will have to be ...
-		if ($("#startTime").val()[1] === ":") {
-			startString += '0';
-			startString += $("#startTime").val()[0];
+
+		var fillOutStartTime = function () {
 			startString += ':';
-			startString += $("#startTime").val()[2];
-			startString += $("#startTime").val()[3];
+			startString += gigStart.val()[2];
+			startString += gigStart.val()[3];
 			startString += ':00';
 		}
-		else {
-			startString += $("#startTime").val()[0];
-			startString += $("#startTime").val()[1];
+
+		var fillOutStartTimeLong = function () {
 			startString += ':';
-			startString += $("#startTime").val()[3];
-			startString += $("#startTime").val()[4];
+			startString += gigStart.val()[3];
+			startString += gigStart.val()[4];
 			startString += ':00';
 		}
-		// startString += ':';
-		// startString += gigMinuteStart;
-		// startString += ':00';
+
+		if (gigStart.val()[1] === ":") {
+			if (gigStart.val()[4] === "a") {
+				startString += '0';
+				startString += gigStart.val()[0];
+				fillOutStartTime();
+			}
+			else if (gigStart.val()[4] === "p") {
+				if (gigStart.val()[0] === "1") {
+					startString += '13';
+					fillOutStartTime();
+				}
+				else if (gigStart.val()[0] === "2") {
+					startString += '14';
+					fillOutStartTime();
+				}
+				else if (gigStart.val()[0] === "3") {
+					startString += '15';
+					fillOutStartTime();
+				}
+				else if (gigStart.val()[0] === "4") {
+					startString += '16';
+					fillOutStartTime();
+				}
+				else if (gigStart.val()[0] === "5") {
+					startString += '17';
+					fillOutStartTime();
+				}
+				else if (gigStart.val()[0] === "6") {
+					startString += '18';
+					fillOutStartTime();
+				}
+				else if (gigStart.val()[0] === "7") {
+					startString += '19';
+					fillOutStartTime();
+				}
+				else if (gigStart.val()[0] === "8") {
+					startString += '20';
+					fillOutStartTime();
+				}
+				else if (gigStart.val()[0] === "9") {
+					startString += '21';
+					fillOutStartTime();
+				}
+			}
+		}
+		if (gigStart.val()[2] === ":") {
+			if (gigStart.val()[5] === "a") {
+				if (gigStart.val()[0] === "1" && gigStart.val()[1] === "2") {
+					startString += '00';
+					fillOutStartTimeLong();
+					//i.e midnight
+				}
+				else if (gigStart.val()[0] === "1" && gigStart.val()[1] === "1") {
+					startString += '11';
+					fillOutStartTimeLong();
+					//i.e. 11am
+				}
+				else if (gigStart.val()[0] === "1" && gigStart.val()[1] === "0") {
+					startString += '10';
+					fillOutStartTimeLong();
+					//i.e. 10am
+				}
+			}
+			else if (gigStart.val()[5] === "p") {
+				if (gigStart.val()[1] === "0") {
+					startString += '22';
+					fillOutStartTimeLong();
+						//i.e. 10pm
+				}
+				else if (gigStart.val()[1] === "1") {
+					startString += '23';
+					fillOutStartTimeLong();
+					console.log("hi")
+					//i.e. 11pm
+				}
+				else if (gigStart.val()[1] === "2") {
+					startString += '12';
+					fillOutStartTimeLong();
+					//i.e. 12pm
+					}
+				}
+			}
 		console.log(startString);
-		//console.log(gigHourStart);
-		//gigYearStart + "-" + gigMonthStart + "-" gigDayStart + "T" + gigHourStart + ":" + gigMinuteStart + ":" + gigSecondStart;
+
+		var endString = '';
+		endString += gigYear;
+		endString += '-';
+		endString += gigMonth;
+		endString += '-';
+		endString += gigDay;
+		endString += 'T';
+
+		// if ($("#endTime").val()[1] === ":") {
+		// 	startString += '0';
+		// 	startString += $("#startTime").val()[0];
+		// 	startString += ':';
+		// 	startString += $("#startTime").val()[2];
+		// 	startString += $("#startTime").val()[3];
+		// 	startString += ':00';
+		// }
+		// else {
+		// 	startString += $("#startTime").val()[0];
+		// 	startString += $("#startTime").val()[1];
+		// 	startString += ':';
+		// 	startString += $("#startTime").val()[3];
+		// 	startString += $("#startTime").val()[4];
+		// 	startString += ':00';
+		// }
 
 
 		var newGig = {
