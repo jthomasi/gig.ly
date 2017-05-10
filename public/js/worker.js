0a4a66142918fcb4425b78d4d9daee919de8edad
Jacob Wallace <jacob.wallace@utexas.edu>
2017-05-10 00:26:55 -0500

    cleaned it up

--- public/js/worker.js
+++ public/js/worker.js
@@ -7,34 +7,35 @@ $(document).ready(function(){
 		            description: 'sgsegs',
 		            duration: '4',
 		            location: 'Houston, TX',
+		            gigTaken: false,
 		            allDay : false // will make the time show
 		        },
 
 		        {
-		            title  : "Test Gig2",
+		            title  : "Tasting",
 		            start  : '2017-06-07T12:30:00',
 		            description: 'segse',
 		            duration: '3',
 		            location: 'HEB (1000 E 41st St)',
+		            gigTaken: true,
 		            allDay : false // will make the time show
 		        },
 
 		        {
-		            title  : "Test Gig2",
+		            title  : "Demo",
 		            start  : '2017-06-07T20:30:00',
 		            description: 'segse',
 		            duration: '2',
 		            location: 'Sprouts',
+		            gigTaken: false,
 		            allDay : false // will make the time show
 		        }
 		    ]
 
-	//need function for reading out date nicely and time
-
 	var jobs = $("#jobs");
 
 	//if there are no gigs scheduled
-	if (gigArray.length < 0) {
+	if (gigArray.length < 1) {
 		jobs.append('<div class = "notification is-warning">Your gig employer has not scheduled any gigs!</div>');
 	}
 
@@ -47,6 +48,13 @@ $(document).ready(function(){
 		var standardHour;
 		var militaryHour = gigArray[i].start[11] + gigArray[i].start[12];
 		var noon;
+		var displayStyle;
+
+		//could do whole string this way that way we wouldn't have to append button to booked gigs
+		if (gigArray[i].gigTaken === true)
+			displayStyle = '<div class = "notification is-warning">' + gigId + ". GIG BOOKED! ";
+		else
+			displayStyle = '<div class = "notification is-primary">' + gigId + ". ";
 
 		if (militaryHour >= 12)
 			noon = "p";
@@ -68,18 +76,22 @@ $(document).ready(function(){
 
 		console.log(standardHour);
 
-		jobs.append('<div class = "notification is-primary">'
-		+ gigId + ". "
+		//append all data from gigArray onto html page
+		jobs.append(displayStyle
 		+ gigArray[i].title + " || "
-		+ gigArray[i].location + " || "
+		+ gigArray[i].location + "<br>"
 		+ gigArray[i].start[5] + gigArray[i].start[6] + " / "
 		+ gigArray[i].start[8] + gigArray[i].start[9] + " / "
 		+ gigArray[i].start[0] + gigArray[i].start[1] + gigArray[i].start[2] + gigArray[i].start[3]
 		+ " || " + standardHour + ":"
-		+ gigArray[i].start[14] + gigArray[i].start[15] + noon + " || "
+		+ gigArray[i].start[14] + gigArray[i].start[15] + noon + "<br>"
 		+ "This gig is approximately " + gigArray[i].duration + " hours long."
 		+ '</div><br>');
 	}
 }
+
+//still need to append a dynamic button for accepting gigs
+//2 buttons, one for showing description of event and other is for accepting
+//should pop up a modal that twilio uses to send to ADMIN's phone number
 
 });
